81954ffc9292ece0a7119941566ead21eb3aa528
Gediminas Milašius <gediminasmilasius@gmail.com>
2021-12-28 22:41:06 +0200

    unpriv attempt

--- Dockerfile
+++ Dockerfile
@@ -3,21 +3,20 @@ FROM debian:buster-slim
 RUN apt update && \
     apt install -y curl wget tmux cron procps
 
-RUN cron &
-
 RUN addgroup --gid 1000 terraria && \
-    adduser --system --shell /sbin/nologin --uid 1000 --ingroup terraria terraria && \
-    ln -s /home/terraria/.local/share/Terraria /data
-USER terraria
+    adduser --system --shell /dev/null --uid 1000 --ingroup terraria terraria && \
+    ln -s /home/terraria/.local/share/Terraria /data && \
+    install -o 1000 -g 1000 -d /server
 
-VOLUME /data
-WORKDIR /server
-
+COPY --chown=1000:1000 start.sh /
 COPY --chown=1000:1000 entrypoint.sh /
 COPY --chown=1000:1000 run.sh /usr/local/bin/run
 
 EXPOSE 7777
 ENV TMLSERVER_AUTOSAVE_INTERVAL="*/10 * * * *"
 
-ENTRYPOINT [ "/entrypoint.sh" ]
-# ENTRYPOINT [ "tail", "/dev/null", "-f" ]
+VOLUME [ "/server" ]
+VOLUME [ "/data" ]
+
+#ENTRYPOINT [ "/entrypoint.sh" ]
+ ENTRYPOINT [ "tail", "/dev/null", "-f" ]
---
+++ start.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+cd /server || exit
+
+STDOUT_PIPE=/tmp/tmod.out
+
+function shutdown() {
+    run "exit"
+    while [ -e "/proc/$TMLS_PID" ]; do
+        sleep .2
+    done
+    rm $STDOUT_PIPE
+}
+
+if [ "$TMLSERVER_VERSION" != "$(cat current_version)" ]; then
+    GITHUB_URL='https://github.com/tModLoader/tModLoader/releases'
+
+    LOADER_TAR_FILE_NAME="tModLoader.Linux.v$TMLSERVER_VERSION.tar.gz"
+    LOADER_DL_URL="https://github.com/tModLoader/tModLoader/releases/download/v$TMLSERVER_VERSION/$LOADER_TAR_FILE_NAME"
+
+    if [ "$(curl -I "$LOADER_DL_URL" -s -o /dev/null -w "%{http_code}")" -ge 400 ]; then
+        echo "Unknown loader version. See latest version at $GITHUB_URL/latest. Do not include the v before the number."
+        exit 1
+    fi
+
+    wget -c "$LOADER_DL_URL"
+    tar -xz -f "$LOADER_TAR_FILE_NAME"
+
+    echo "$TMLSERVER_VERSION" > current_version
+fi
+
+# Setup autosave
+(crontab -l 2>/dev/null; echo "$TMLSERVER_AUTOSAVE_INTERVAL /usr/local/bin/run 'say Autosaving' && /usr/local/bin/run 'save'") | crontab -
+
+# Custom logic to handle the setup
+if [ "$1" = "setup" ]; then
+    exec ./tModLoaderServer
+fi
+
+trap shutdown SIGTERM SIGINT
+
+mkfifo $STDOUT_PIPE
+tmux new-session -d "./tModLoaderServer -config serverconfig.txt | tee $STDOUT_PIPE" &
+
+while [ -z "$TMLS_PID" ]; do
+    TMLS_PID=$(pgrep tModLoader)
+    sleep .2
+done
+
+tail --pid "$TMLS_PID" -n +1 -f /data/ModLoader/Logs/server.log &
+cat $STDOUT_PIPE &
+wait ${!}
